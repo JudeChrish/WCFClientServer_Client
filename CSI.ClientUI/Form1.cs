cbd75d80a5f6356b352aa6302f3c416530f63a73
Your Name <you@example.com>
2017-11-07 09:49:35 +0530

    Add project files.

---
+++ CSI.ClientUI/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSI.ClientUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomerServiceClient client = new CustomerServiceClient("BasicHttpBinding_ICustomerService");
+
+            dataGridView1.DataSource = client.GetAllCustomers();
+        }
+}
+}
